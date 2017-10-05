1420dbb35555c070fd392fa6d458238d93422117
LukeStro47 <lukestrong47@gmail.com>
2017-10-05 18:43:01 -0400

    Sign Up Form Done

--- signup.js
+++ signup.js
@@ -1,30 +1,18 @@
 // Initialize Firebase
 (function(){
     var config = {
-        apiKey: "AIzaSyCrfbcS3Ut5BoyCfNaX-WPUjmglB8cf1kE",
-        authDomain: "game-jumper.firebaseapp.com",
-        databaseURL: "https://game-jumper.firebaseio.com",
-        projectId: "game-jumper",
-        storageBucket: "game-jumper.appspot.com",
-        messagingSenderId: "1076316179864"
+        apiKey: "AIzaSyBsnqMW3L_2pjboOOzZJ8fJDYzGgCxcyoo",
+        authDomain: "game-jumper-tester.firebaseapp.com",
+        databaseURL: "https://game-jumper-tester.firebaseio.com",
+        projectId: "game-jumper-tester",
+        storageBucket: "game-jumper-tester.appspot.com",
+        messagingSenderId: "457793690679"
     };
     firebase.initializeApp(config);
     //Get Elements
     const txtEmail = document.getElementById('txtEmail');
     const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
     const btnSignUp = document.getElementById('btnSignUp');
-    const btnLogOut = document.getElementById('btnLogOut');
-    //Add Login Event
-    btnLogin.addEventListener('click', e => {
-        //Get Email & Pass
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        //Sign In
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
-    });
     //Add SignUp Event
     btnSignUp.addEventListener('click', e => {
         //Get Email & Pass
